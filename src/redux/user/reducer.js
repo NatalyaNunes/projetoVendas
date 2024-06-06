023d53fdf6e319535a6860c22bf024d546bd56e5
Natalya Nunes <nunes.natalya23@gmail.com>
2024-06-06 14:05:24 -0300

    ajustes redux

---
+++ src/redux/user/reducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+    CurrentUser:null,
+}
+    
+const userReducer = (state = initialState, action) => {
+    if(action.type == 'user/login'){
+        return {... state, currentUser:10};
+    }
+    return state;
+};
+
+export default userReducer;
