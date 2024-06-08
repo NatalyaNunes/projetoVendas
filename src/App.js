20b66c8b75fb73990c7931e8c2d2b691fc4aca98
Natalya Nunes <nunes.natalya23@gmail.com>
2024-06-08 09:17:15 -0300

    private routes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Cadastro from "./components/forms/Cadastro";
 import Table from "./components/Table";
 import Login from "./components/Login";
 import Home from "./components/Home";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
@@ -14,10 +14,9 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Login/>}/>
-          <Route path="/home" element={<Home/>}/>
-          <Route path="/cadastro" element={<Cadastro/>}/>
-          <Route path="/sidebar" element={<SideBar/>}/>
-          <Route path="/vendas" element={<Table/>}/>
+          <Route path="/home" element={ <PrivateRoute><Home/></PrivateRoute>}/>
+          <Route path="/cadastro" element={<PrivateRoute><Cadastro/></PrivateRoute>}/>
+          <Route path="/vendas" element={<PrivateRoute><Table/></PrivateRoute>}/>
         </Routes>
       </div>
     </Router>
